251a00d87ce7b79cbbb7a677a491768c2699738d
Star00Star <118890602+Star00Star@users.noreply.github.com>
2024-05-18 19:26:28 +0800

    Update model.py

--- model.py
+++ model.py
@@ -8,26 +8,26 @@ class Config(object):
     """配置参数"""
     def __init__(self):
         self.model_name = 'bert'
-        self.save_path = '/content/drive/MyDrive/SpamClassifier/checkpoint/' + self.model_name + '.ckpt'        # 模型训练结果
-        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')   # 设备
-        self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
-        self.num_classes = 2                                            # 标签类别数
+        self.save_path = 'checkpoint/' + self.model_name + '.ckpt'
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.require_improvement = 1000
+        self.num_classes = 2
         self.max_length = 128
         self.batch_size = 16
-        self.num_epochs = 3                                            # epoch数
-        self.learning_rate = 5e-5                                       # 学习率
-        self.bert_path = '/content/drive/MyDrive/SpamClassifier/pretrain_model/bert-base-chinese'
+        self.num_epochs = 3
+        self.learning_rate = 5e-5
+        self.bert_path = 'bert-base-chinese'  # 修改这里为 Hugging Face Hub 上的模型名称
         self.hidden_size = 768
 
 
 class MyModel(nn.Module):
 
-    def __init__(self, config):
+    def __init__(self, Config):
         super(MyModel, self).__init__()
-        self.bert = BertModel.from_pretrained('/content/drive/MyDrive/SpamClassifier/pretrain_model/bert-base-chinese')
+        self.bert = BertModel.from_pretrained(Config.bert_path)
         for param in self.bert.parameters():
             param.requires_grad = True
-        self.fc = nn.Linear(config.hidden_size, config.num_classes)
+        self.fc = nn.Linear(Config.hidden_size, Config.num_classes)
 
     def forward(self, x):
         input_ids = x['input_ids']  # 输入的句子每个词对应的id
