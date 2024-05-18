32517a8b206ffb4e6070b6ea9c393fe7cf18b9a9
Star00Star <118890602+Star00Star@users.noreply.github.com>
2024-05-18 18:30:49 +0800

    Update model.py

--- model.py
+++ model.py
@@ -24,7 +24,7 @@ class MyModel(nn.Module):
 
     def __init__(self, config):
         super(MyModel, self).__init__()
-        self.bert = BertModel.from_pretrained(config.bert_path)
+        self.bert = BertModel.from_pretrained('/content/drive/MyDrive/SpamClassifier/pretrain_model/bert-base-chinese')
         for param in self.bert.parameters():
             param.requires_grad = True
         self.fc = nn.Linear(config.hidden_size, config.num_classes)
